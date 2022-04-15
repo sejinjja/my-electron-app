2c5a94aec3b94efde15e53c1a3ae59e395958af5
sejin <sejinjja@gmail.com>
2022-04-16 01:55:50 +0900

    clipboard 기능 추가.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, clipboard, ipcMain} = require('electron')
 const path = require('path')
+let lastReadClipboard = ''
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -13,6 +13,21 @@ const createWindow = () => {
 
   win.loadURL('http://localhost:8080')
   win.webContents.openDevTools()
+  ipcMain.on('init', () => {
+    lastReadClipboard = clipboard.readText()
+    win.webContents.send('onChangeClipboard', {type: 'text', value: lastReadClipboard})
+  })
+  clipboardTextWatcher(win)
+}
+
+function clipboardTextWatcher(win) {
+  setInterval(() => {
+    const currentReadClipboard = clipboard.readText()
+    if (currentReadClipboard !== lastReadClipboard) {
+      win.webContents.send('onChangeClipboard', {type: 'text', value: currentReadClipboard})
+      lastReadClipboard = currentReadClipboard
+    }
+  }, 700)
 }
 
 app.whenReady().then(() => {
--- preload.js
+++ preload.js
@@ -1,7 +1,8 @@
-
-const { contextBridge }  = require('electron')
+const { contextBridge, clipboard, ipcRenderer }  = require('electron')
 const fs = require('fs').promises
 const path = require('path')
+
+const validChannels = ['onChangeClipboard', 'init']
 
 contextBridge.exposeInMainWorld(
   'api', {
@@ -10,7 +11,46 @@ contextBridge.exposeInMainWorld(
     },
     readFile(fileName, ...args) {
       return fs.readFile(path.join(__dirname, fileName), ...args)
-    }
+    },
+  },
+)
+
+contextBridge.exposeInMainWorld(
+  'ipc', {
+    send: (channel, data) => {
+      if (validChannels.includes(channel)) {
+        ipcRenderer.send(channel, data)
+      }
+    },
+    on: (channel, func) => {
+      if (validChannels.includes(channel)) {
+        ipcRenderer.on(channel, (event, ...args) => func(...args))
+      }
+    },
+  }
+)
+
+contextBridge.exposeInMainWorld(
+  'clipboardHandler', {
+    // availableFormats: clipboard.availableFormats,
+    // clear: clipboard.clear,
+    // has: clipboard.has,
+    // read: clipboard.read,
+    // readBookmark: clipboard.readBookmark,
+    // readBuffer: clipboard.readBuffer,
+    // readFindText: clipboard.readFindText,
+    // readHTML: clipboard.readHTML,
+    // readImage: clipboard.readImage,
+    // readRTF: clipboard.readRTF,
+    readText: clipboard.readText,
+    // write: clipboard.write,
+    // writeBookmark: clipboard.writeBookmark,
+    // writeBuffer: clipboard.writeBuffer,
+    // writeFindText: clipboard.writeFindText,
+    // writeHTML: clipboard.writeHTML,
+    // writeImage: clipboard.writeImage,
+    // writeRTF: clipboard.writeRTF,
+    writeText: clipboard.writeText,
   }
 )
 
