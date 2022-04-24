f32b90c2a2c925d09ae9424eeae6f1349449d6c3
sejinjja <sejinjja@naver.com>
2022-04-24 17:02:25 +0900

    v.1.0.0

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
+const { autoUpdater } = require('electron-updater')
+const log = require('electron-log')
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -20,6 +22,38 @@ const createWindow = () => {
 }
 
 app.whenReady().then(() => {
-  createWindow()
+  autoUpdater.checkForUpdates()
 })
 
+autoUpdater.autoDownload = false
+
+autoUpdater.on('checking-for-update', () => {
+  log.info('업데이트 확인 중...')
+})
+autoUpdater.on('update-available', (info) => {
+  log.info('업데이트가 가능합니다.')
+  autoUpdater.downloadUpdate()
+})
+autoUpdater.on('update-not-available', (info) => {
+  log.info('현재 최신버전입니다.')
+  createWindow()
+})
+autoUpdater.on('error', (err) => {
+  log.info('에러가 발생하였습니다. 에러내용 : ' + err)
+  // createWindow()
+  app.quit()
+})
+autoUpdater.on('download-progress', (progressObj) => {
+  log.info('progressObj', progressObj)
+})
+autoUpdater.on('update-downloaded', (info) => {
+  log.info(info)
+  log.info('업데이트가 완료되었습니다.')
+  setImmediate(() => {
+    autoUpdater.quitAndInstall()
+  })
+})
+
+app.on('window-all-closed', () => {
+  app.quit()
+})
